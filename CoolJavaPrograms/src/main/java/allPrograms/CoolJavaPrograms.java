8e600d6bf71dedbcbe65b79d90cdb8f2ca0f2a85
MaruthiSikhakolli <maruthismith@gmail.com>
2022-04-19 15:44:48 +0530

    sdf

--- CoolJavaPrograms/src/main/java/allPrograms/CoolJavaPrograms.java
+++ CoolJavaPrograms/src/main/java/allPrograms/CoolJavaPrograms.java
@@ -83,12 +83,13 @@ public class CoolJavaPrograms {
 			System.out.print("Given number is not a palindrome number");
 	}
 
-	//Method to print first 'n' Fibonacci numbers using recursive approach from the series of infinite Fibonacci numbers starting from 0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765 10946 17711 28657 46368 75025 121393 196418 317811 514229 832040 1346269 2178309 ..
+	//Method to print first 'n' Fibonacci numbers using non recursive approach from the series of infinite Fibonacci numbers starting from 0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765 10946 17711 28657 46368 75025 121393 196418 317811 514229 832040 1346269 2178309 ..
 	public void fibonacciSeries(int num) {
 		int a = 0, b = 0, c = 1;
 
 		for (int i = 1; i <= num; i++) {
 			a = b;
+			
 			b = c;
 			c = a + b;
 			System.out.print(a + " ");
